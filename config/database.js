da048e85a84a2e52a5221d9c354a5735c99625fc
Kumar Shashank <kshashank23012000@gmail.com>
2023-01-08 00:00:16 +0530

    initial commit

--- config/database.js
+++ config/database.js
@@ -1,11 +1,13 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'dpg-cesrbko2i3mh51vhj670-a.oregon-postgres.render.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi_db_wr8x'),
+      user: env('DATABASE_USERNAME', 'admin'),
+      password: env('DATABASE_PASSWORD', 'or0hCjW95gkM04M0cYR7YsNOKj645Aow'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
-    useNullAsDefault: true,
   },
 });
